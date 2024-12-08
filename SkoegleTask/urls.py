0791d5ed87fa4024e17c284fc3e0ad9ec5938e9a
murali <gollamuralikrishnayadav45@gmail.com>
2024-12-08 17:35:51 +0530

    new change

--- SkoegleTask/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='Home'),
-    path('admin/', admin.site.urls),
-    path('', include(('videos.urls', 'videos'), namespace='videos')),
-]
--- videos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'videos'
-
-urlpatterns = [
-    path('list/', views.list_videos, name='list_videos'),
-    path('upload/', views.upload_video, name='upload_video'),
-]
